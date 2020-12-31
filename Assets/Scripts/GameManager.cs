f680258786af4d4ecd10ccd5609b39adcce487a4
François Egner <egner.francois@outlook.com>
2020-12-31 16:52:02 +0100

    Auto sorting of pre added sceneObjects
    SceneObject added in the editor will now be sorted automatically when SpriteSorter starts. Thanks to KaeGore @ Vuvuu Inc. for this idea.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    [SerializeField] GameObject startPos;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start(){
+        Instantiate(player, startPos.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
